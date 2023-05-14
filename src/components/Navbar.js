9cebba3fd0c1dda42a262ba34b9d86233a0b198b
Sushant Sharma <sushantsharma@Sushants-Air.bbrouter>
2023-05-15 00:03:52 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,16 +10,16 @@ function Navbar() {
     <Stack
       direction="row"
       justifyContent="space-around"
-      sx={{ gap: { sm: "122px", xs: "20px" }, mt: { sm: "32px", xs: "20px" }, justifyContent: "none" }}  px="20px"
+      sx={{ gap: { sm: "122px", xs: "15px" }, mt: { sm: "32px", xs: "20px" }, justifyContent: "none" }}  px="20px"
     >
       <Link to="/">
         <img
           src={Logo}
           alt="logo"
-          style={{ width: "48px", height: "48px", margin: "0 20px" }}
+          style={{ width: "48px", height: "48px", margin: "0 2px" }}
         />
       </Link>
-      <Stack direction="row" fontSize="24px" alignItems="flex-end" sx={{ gap: { sm: "50px", xs: "40px"}}}>
+      <Stack direction="row" fontSize="24px" alignItems="flex-end" sx={{ gap: { sm: "50px", xs: "20px"}}}>
         <Link
           to="/"
           style={{
