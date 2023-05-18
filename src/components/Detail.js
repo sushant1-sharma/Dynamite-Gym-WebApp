52f2d36163cc5e80d8a3900dad941fb61fa0bc78
Sushant Sharma <sushantsharma@Sushants-Air.bbrouter>
2023-05-19 02:07:00 +0530

    latest commit

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -31,7 +31,7 @@ function Detail({ exerciseDetail }) {
     >
       <img src={gifUrl} alt={name} Loading="lazy" className="detail-image" />
       <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
-        <Typography variant="h3">{name}</Typography>
+        <Typography variant="h3" textTransform="capitalize">{name}</Typography>
         <Typography variant="h6">
           Exercises keep you strong. {name} {` `}
           is one of the best exercises to target your {target}. It will help you
